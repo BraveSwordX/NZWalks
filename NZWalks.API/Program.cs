a321635651dfdbc363b15eb224bef14df94579f1
Supachai Jitnuang <supachai.jitnuang@gmail.com>
2024-10-09 22:38:34 +0700

    35. Implement Repository Pattern In ASP.NET Core Web APIs (จบหัวช้อนี้)

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
-    // ชื่อ "NZWalksDbContext" ใช้จาก ConnectionStrings ใน appsettings.json
+// ชื่อ "NZWalksDbContext" ใช้จาก ConnectionStrings ใน appsettings.json
+
+builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 
 var app = builder.Build();
 
